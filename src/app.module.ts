5e300d5a5814175debacfc5b57b8e27a000d6d2b
naske <naske777ig@gmail.com>
2024-06-18 23:14:56 -0400

    Agregar el modulo de money

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoneyModule } from './money/money.module';
 
 @Module({
-  imports: [],
+  imports: [MoneyModule],
   controllers: [AppController],
   providers: [AppService],
 })
